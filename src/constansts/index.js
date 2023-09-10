30b62edec538b7454a330050365afc647c99d501
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-10 19:30:42 +0530

    Removed experience

--- src/constansts/index.js
+++ src/constansts/index.js
@@ -143,19 +143,6 @@ import {
         "The program helped me enhance my communication skills, This experience was beyond my wildest imagination",
       ],
     },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    }
   ];
   
   const testimonials = [
@@ -166,22 +153,6 @@ import {
       designation: "Student",
       company: "Acharya",
       image: "https://res.cloudinary.com/dffhtczwf/image/upload/v1694271419/Portfolio21/user.name/j2vk3tt7lz9bx6imt3mk.png",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rishav optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
   
